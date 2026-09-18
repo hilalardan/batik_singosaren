f2e55411c3ad913c688b3df345cb3a8f117aa254
hilalardan <hilalardan6@gmail.com>
2026-09-18 13:07:49 +0700

    Update hooks.js

--- src/hooks.js
+++ src/hooks.js
@@ -2,7 +2,7 @@
 import { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://batiksingosaren-production.up.railway.app";
 
 // ===============================
 // DATA BERANDA
