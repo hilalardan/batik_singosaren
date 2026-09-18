6fa10493163cead7facc2bcaec35f16f6b3af568
hilalardan <hilalardan6@gmail.com>
2026-09-18 10:15:28 +0700

    Create constants.js

---
+++ src/constants.js
@@ -0,0 +1,58 @@
+export const SITE = {
+  logo_toko: '/logo.png',
+  nama_toko: 'Batik Singosaren',
+  tentang:'Batik Singosaren menyediakan berbagai pilihan batik dengan motif khas, kualitas terbaik, dan harga yang terjangkau untuk berbagai kebutuhan.',
+  foto_banner: '',
+  alamat_toko: 'Jl. Niken Gandini, Ponorogo, Jawa Timur',
+  email_toko: 'info@batiksingosaren.com',
+  tlp_toko: 6289516786413,
+  nama_bank_a: 'BCA',
+  nama_bank_b: 'Mandiri',
+  no_rek_a: 1234567890,
+  no_rek_b: 9876543210,
+  jam_buka: 8,
+  jam_tutup: 17,
+  link_wa: 'https://wa.me/6289516786413',
+  link_ig: 'https://instagram.com/',
+  link_fb: 'https://facebook.com/',
+};
+
+export const PUBLIC_NAV = [
+  { to: "/", label: "Beranda", end: true },
+  { to: "/toko", label: "Toko" },
+  { to: "/artikel", label: "Artikel" },
+];
+
+export const KELAMIN = [
+  'Laki-laki',
+  'Perempuan',
+];
+
+export const KATEGORI_PRODUK = [
+  'Batik Tulis',
+  'Batik Cap',
+  'Batik Kombinasi',
+];
+
+export const METODE_BAYAR = [
+  "Cash",
+  "Transfer Bank",
+];
+
+export const SHIPPING = [
+  "Delivery",
+  "Ambil di Toko",
+];
+
+export const STATUS_PROSES = [
+  'Tertunda',
+  'Dikemas',
+  'Dikirim',
+  'Diterima',
+  'Selesai',
+];
+
+export const STATUS_BAYAR = [
+  'Belum',
+  'Dibayar',
+];
