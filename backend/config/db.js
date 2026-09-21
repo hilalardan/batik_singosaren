5682b89701e163a359f080028a5891a7d550d664
hilalardan <hilalardan6@gmail.com>
2026-09-21 16:48:09 +0700

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,27 +1,21 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createPool({
+const db = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASS || "",
   database: process.env.DB_NAME || "batik_singosaren",
-  port: process.env.DB_PORT || 3306,
-
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
 });
 
-db.getConnection((err, connection) => {
+db.connect((err) => {
   if (err) {
     console.error("Koneksi database gagal:", err.message);
     return;
   }
 
-  console.log("Terhubung ke MySQL Railway");
-
-  connection.release();
+  console.log(`Terhubung ke MySQL (${process.env.DB_NAME})`);
 });
 
 module.exports = db;
