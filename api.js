aecff1d3665329c293ff3cdc8e2eebbff8cc49e9
hilalardan <hilalardan6@gmail.com>
2026-09-21 17:04:53 +0700

    Update api.js

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 export const API_BASE =
-  import.meta.env.VITE_API_URL || "http://localhost:5000";
+  import.meta.env.VITE_API_URL || "https://batiksingosaren-production.up.railway.app";
 
 export function authHeaders() {
   const token = localStorage.getItem("toko_token");
